c0b240f21f3546fd71eb34fce2e758d61994206e
Anton Gadiyatov <gadiyatov.an@gmail.com>
2021-11-09 15:34:31 +0300

    027 add monsters names

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,24 +6,29 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hello World!',
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asd',
+        },
+        {
+          name: 'Zombie',
+          id: 'asd2',
+        },
+        {
+          name: 'Drakula',
+          id: 'asd3',
+        },
+      ],
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button
-            onClick={() => {
-              this.setState({ string: 'Hello Anton!' });
-            }}
-          >
-            Change Text!
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
       </div>
     );
   }
